3c77041a6c5e54003d6095c41a57c4d685b9442a
Angus <angus.goody@gmail.com>
2025-02-26 00:08:13 +0000

    Init

---
+++ src/components/Logo.tsx
@@ -0,0 +1,171 @@
+import { useId } from 'react'
+
+export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
+  let id = useId()
+
+  return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          style={{
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 2,
+          }}
+          viewBox="0 0 3461 801"
+          {...props}
+      >
+        <g transform="matrix(.9774 0 0 1 -2342.465 -2673.5)">
+          <path
+              d="M2396.61 2673.5h3540.97v800.002H2396.61z"
+              style={{
+                fill: "none",
+              }}
+          />
+          <clipPath id="a">
+            <path d="M2396.61 2673.5h3540.97v800.002H2396.61z"/>
+          </clipPath>
+          <g clipPath="url(#a)">
+            <path
+                d="M.094 0h.284C.58.004.753-.155.75-.349.753-.545.58-.704.378-.7H.094V0Zm.28-.584c.067 0 .125.023.172.068a.22.22 0 0 1 .071.166.22.22 0 0 1-.071.166.24.24 0 0 1-.172.067H.225v-.467h.149Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 3053.499 3177.338)"
+            />
+            <path
+                d="M.252-.662a.358.358 0 0 0-.135.493.372.372 0 0 0 .322.179.374.374 0 0 0 .186-.048.355.355 0 0 0 .136-.493.372.372 0 0 0-.322-.179.377.377 0 0 0-.187.048Zm.187.074c.134 0 .236.107.236.238a.236.236 0 1 1-.472 0c0-.066.023-.122.068-.168a.223.223 0 0 1 .168-.07Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 3384.14 3177.338)"
+            />
+            <path
+                d="M.435-.71a.363.363 0 0 0-.367.36c0 .199.164.36.367.36a.371.371 0 0 0 .293-.143L.615-.197a.217.217 0 0 1-.179.085.223.223 0 0 1-.166-.069.235.235 0 0 1-.067-.169.23.23 0 0 1 .067-.168.22.22 0 0 1 .166-.07c.074 0 .134.029.179.086l.113-.064A.37.37 0 0 0 .435-.71Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 3733.97 3177.338)"
+            />
+            <path
+                d="M1.095-.7H.949l-.163.5-.158-.5h-.13l-.157.5-.164-.5H.032l.235.7h.135l.161-.498L.724 0H.86l.235-.7Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 4051.597 3177.338)"
+            />
+            <path
+                d="M.64-.7H.094V0h.554v-.117H.225v-.181H.61V-.41H.225v-.173H.64V-.7Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 4478.213 3177.338)"
+            />
+            <path
+                d="M.497-.7h-.17L.032 0h.155l.064-.154h.323L.638 0h.156L.497-.7Zm-.085.144.127.298H.286l.126-.298Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 4770.762 3177.338)"
+            />
+            <path
+                d="M.773-.7H.617l-.215.548L.187-.7H.032l.284.7h.171l.286-.7Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 5070.797 3177.338)"
+            />
+            <path
+                d="M.64-.7H.094V0h.554v-.117H.225v-.181H.61V-.41H.225v-.173H.64V-.7Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 5397.718 3177.338)"
+            />
+            <path
+                d="M.094 0h.131v-.24H.37l.044-.001L.575 0h.15L.55-.258C.657-.286.703-.353.703-.47.703-.631.617-.7.401-.7H.094V0Zm.313-.582c.116 0 .16.034.16.121 0 .085-.044.12-.16.12H.225v-.241h.182Z"
+                style={{
+                  fill: "#fff",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(309.58435 0 0 296.65698 5690.282 3177.338)"
+            />
+            <path
+                d="M223.184 800c-11.831 0-23.71-4.506-32.774-13.592-18.102-18.103-18.102-47.398 0-65.5l320.927-320.906L190.41 79.075c-18.102-18.103-18.102-47.423 0-65.5 18.103-18.103 47.424-18.103 65.522 0l353.653 353.653c18.103 18.103 18.103 47.424 0 65.501L255.932 786.404C246.898 795.468 235.041 800 223.184 800Z"
+                style={{
+                  fill: "url(#b)",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(-1.02312 0 0 -1 3071.801 3473.496)"
+            />
+            <path
+                d="M223.184 800c-11.831 0-23.71-4.506-32.774-13.592-18.102-18.103-18.102-47.398 0-65.5l320.927-320.906L190.41 79.075c-18.102-18.103-18.102-47.423 0-65.5 18.103-18.103 47.424-18.103 65.522 0l353.653 353.653c18.103 18.103 18.103 47.424 0 65.501L255.932 786.404C246.898 795.468 235.041 800 223.184 800Z"
+                style={{
+                  fill: "url(#c)",
+                  fillRule: "nonzero",
+                }}
+                transform="matrix(1.02312 0 0 -1 2215.699 3473.496)"
+            />
+          </g>
+        </g>
+        <defs>
+          <linearGradient
+              id="b"
+              x1={0}
+              x2={1}
+              y1={0}
+              y2={0}
+              gradientTransform="matrix(0 -711 711 0 213.602 761.012)"
+              gradientUnits="userSpaceOnUse"
+          >
+            <stop
+                offset={0}
+                style={{
+                  stopColor: "#2f8cb2",
+                  stopOpacity: 1,
+                }}
+            />
+            <stop
+                offset={1}
+                style={{
+                  stopColor: "#0b5777",
+                  stopOpacity: 1,
+                }}
+            />
+          </linearGradient>
+          <linearGradient
+              id="c"
+              x1={0}
+              x2={1}
+              y1={0}
+              y2={0}
+              gradientTransform="matrix(25 -694 -694 -25 233.448 759.012)"
+              gradientUnits="userSpaceOnUse"
+          >
+            <stop
+                offset={0}
+                style={{
+                  stopColor: "#1edbe9",
+                  stopOpacity: 1,
+                }}
+            />
+            <stop
+                offset={1}
+                style={{
+                  stopColor: "#00a8df",
+                  stopOpacity: 1,
+                }}
+            />
+          </linearGradient>
+        </defs>
+      </svg>
+  )
+}
